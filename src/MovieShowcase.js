8b27088eabe1d5b0a57733397ed04ef724657d7b
Bin <binlin35@yahoo.com>
2021-04-26 22:35:38 -0400

    Completed assignment

--- src/MovieShowcase.js
+++ src/MovieShowcase.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import MovieCard from './card-components/MovieCard.js'
 import movieData from './data.js'
+//console.log(movieData)
 
 export default class MovieShowcase extends Component {
 
   generateMovieCards = () => {
     // map over your movieData array and return an array of the correct JSX
+      return movieData.map((date,index) => {
+        return <MovieCard key={index} {...date} />
+      })
   }
 
   render() {
